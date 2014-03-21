f4819b9c9c6f0d45139c436d3f90f0cfd42a5538
rolobio <rolobio@rolobio.com>
2014-03-21 10:41:07 -0600

    Fixing some conventions in sshm/main.py.

--- sshm/main.py
+++ sshm/main.py
@@ -1,4 +1,10 @@
 #! /usr/bin/env python3
+"""
+This module allows the console to use SSHM's functionality.
+
+This module should only be run by the console!
+"""
+
 try: # pragma: no cover version specific
     from lib import sshm
 except ImportError: # pragma: no cover version specific
@@ -23,13 +29,13 @@ def get_argparse_args(args=None):
         from sshm._info import __version__, __long_description__
     import argparse
 
-    p = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
             formatter_class=argparse.RawDescriptionHelpFormatter,
             description=__long_description__)
-    p.add_argument('servers')
-    p.add_argument('command')
-    p.add_argument('--version', action='version', version='%(prog)s '+__version__)
-    args, extra_args = p.parse_known_args(args=args)
+    parser.add_argument('servers')
+    parser.add_argument('command')
+    parser.add_argument('--version', action='version', version='%(prog)s '+__version__)
+    args, extra_args = parser.parse_known_args(args=args)
     return (args, args.command, extra_args)
 
 
@@ -45,14 +51,13 @@ def main(): # pragma: no cover
     args, command, extra_arguments = get_argparse_args()
 
     # Only provided stdin if there is data
-    r_list, w_list, x_list = select.select([sys.stdin], [], [], 0)
+    r_list, i, i = select.select([sys.stdin], [], [], 0)
     if r_list:
         stdin = r_list[0]
     else:
         stdin = None
 
     # Perform the command on each server, print the results to stdout.
-    failure = False
     results = sshm(args.servers, command, extra_arguments, stdin)
     for result in results:
         if ('traceback' in result) and (result['traceback'] != ''):
@@ -65,8 +70,10 @@ def main(): # pragma: no cover
                     result['url'],
                     result['return_code'],
                     ),]
-            if result['stdout']: out.append(result['stdout'].rstrip('\n'))
-            if result['stderr']: out.append(result['stderr'].rstrip('\n'))
+            if result['stdout']:
+                out.append(result['stdout'].rstrip('\n'))
+            if result['stderr']:
+                out.append(result['stderr'].rstrip('\n'))
         # Put output in one line if it can fit
         if sum([s.count('\n') for s in out]) == 0 and len(out) <= 2:
             print(' '.join(out))
