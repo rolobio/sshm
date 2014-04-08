bb32299d67b31c3edacacf77aa0596494f14138a
rolobio <rolobio@rolobio.com>
2014-04-08 16:59:07 -0600

    Slowly converting to the nmap notation.

--- sshm/lib.py
+++ sshm/lib.py
@@ -4,16 +4,51 @@ import subprocess
 import sys
 import threading
 import zmq
+from itertools import product
 from traceback import format_exc
 
 __all__ = ['sshm']
 
 
+_match_ranges = re.compile(r'(?:(\d+)(?:,|$))|(?:(\d+-\d+))')
+
+def expand_ints(to_expand):
+    """
+    Convert a comma-seperated range of integers into a list. Keep any zero
+    padding the numbers may have.
+
+        Example: "1,4,07-10" to ['1', '4', '07', '08', '09', '10']
+
+    @type to_expand: str
+    @param to_expand: Expand this string into a list of integers.
+    """
+    nums = []
+    for single, range_str in _match_ranges.findall(to_expand):
+        if single:
+            nums.append(single)
+        if range_str:
+            i, j = range_str.split('-')
+            # Create a string that will pad the integer with its current amount
+            # of zeroes.
+            # Example: if i is '03' the string will be '%0.2d'
+            padding = '%'+'0.%d' % len(i) +'d'
+            for k in range(int(i), int(j)+1):
+                nums.append(padding % k)
+    return nums
+
 # This is used to check if the target contains any alpha characters.
 _alpha = re.compile(r'[a-zA-Z]')
 
 def is_url(target):
+    """
+    If target conains any alpha characters, it is an URL.
+    """
     return bool(_alpha.search(target))
+
+
+# This is used to parse a range string
+_parse_ranges = re.compile(r'[\[\]]')
+_parse_uri = re.compile(r'[@:]')
 
 
 def target_expansion(input_str):
@@ -21,6 +56,52 @@ def target_expansion(input_str):
     Expand a list of targets into invividual URLs/IPs and their respective
     ports. Preserve any zero-padding the range may contain.
     """
+    targets = []
+    input_list = input_str.split(',')
+    while input_list:
+        # If there is not current target, get one and then continue
+        target = input_list.pop(0)
+
+        if is_url(target):
+            # The current target is a URL
+            if '[' in target:
+                # This URL needs to be expanded
+                while ']' not in target:
+                    # The matching bracket is missing, get more sections until
+                    # it is found.
+                    target += ','+input_list.pop(0)
+                prefix, range_str, suffix = _parse_ranges.split(target)
+                products = [''.join([i,j,k]) for i, j, k in product([prefix,], expand_ints(range_str), [suffix,])]
+                targets.extend(products)
+            else:
+                targets.append(target)
+        else:
+            # The current target is an IP
+            while target.count('.') <= 2:
+                # This IP needs has more parts in the next section.
+                target += ','+input_list.pop(0)
+
+            if '-' in target:
+                # This IP needs to be expanded
+                try:
+                    target, port = target.split(':')
+                except ValueError:
+                    # No port specified
+                    port = None
+                eo = [expand_ints(i) for i in target.split('.')]
+                products = ['.'.join([i,j]) for i,j in product(eo[2], eo[3])]
+                products = ['.'.join([i,j]) for i,j in product(eo[1], products)]
+                products = ['.'.join([i,j]) for i,j in product(eo[0], products)]
+                # Add the port back on if it was specified
+                if port:
+                    targets.extend([p+':'+port for p in products])
+                else:
+                    targets.extend(products)
+            else:
+                # This IP is complete
+                targets.append(target)
+
+    return targets
 
 
 def popen(cmd, stdin, stdout, stderr): # pragma: no cover
@@ -149,6 +230,7 @@ def sshm(servers, command, extra_arguments=None, stdin=None):
     @returns: A list containing (success, handle, message) from each method
         call.
     """
+    return
     context = zmq.Context()
     # The results of each ssh call is reported to this sink
     sink = context.socket(zmq.PULL)
